527006863a7a7b671850784127ed76c84e30a550
Sam <samme.s90@gmail.com>
2024-12-13 12:32:13 +0100

    Add colors

--- setup.sh
+++ setup.sh
@@ -1,50 +1,67 @@
 #!/bin/bash
 
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+# Function to print colored messages
+print_colored() {
+    local color=$1
+    local message=$2
+    echo -e "${color}${message}${NC}"
+}
+
 DIR="$HOME/dev"
 GIT_REPO="setup"
 GIT_URI="git@github.com:samme90s/$GIT_REPO.git"
 
-echo $DIR
-echo $GIT_REPO
-echo $GIT_URI
+print_colored $BLUE "Directory: $DIR"
+print_colored $BLUE "Git Repository: $GIT_REPO"
+print_colored $BLUE "Git URI: $GIT_URI"
 
 # Public tools
 #
 # Update and upgrade
-echo "Installing public tools"
-echo "Updating and upgrading..."
+print_colored $YELLOW "Installing public tools"
+print_colored $YELLOW "Updating and upgrading..."
 
 sudo apt-get update &&
     sudo apt-get upgrade -y &&
-    echo "Update and upgrade complete"
+    print_colored $GREEN "Update and upgrade complete"
 
 # Install Bat
-echo "Installing Bat..."
+print_colored $YELLOW "Installing Bat..."
 
 sudo apt install bat -y &&
-    echo "Bat installed"
+    print_colored $GREEN "Bat installed"
 
 # Setup
 #
 # Check if the directory exists
 if [ -d $DIR ]; then
-    echo "Directory exists"
+    print_colored $GREEN "Directory exists"
 else
-    echo "Directory does not exist. Creating..."
+    print_colored $RED "Directory does not exist. Creating..."
     mkdir $DIR &&
-        echo "Directory created"
+        print_colored $GREEN "Directory created"
 fi
 
-echo "Moving to '$DIR'..."
-cd $DIR
+cd $DIR &&
+    print_colored $GREEN "Moved to '$DIR'"
 
 # Check if the directory is empty
 if [ "$(ls -A $DIR)" ]; then
-    echo "Directory is not empty. Skipping cloning..."
+    print_colored $YELLOW "Directory is not empty. Skipping cloning..."
 else
-    echo "Directory is empty. Cloning..."
+    print_colored $YELLOW "Directory is empty. Cloning..."
     git clone $GIT_URI &&
-        echo "Cloned '$GIT_REPO'"
+        print_colored $GREEN "Cloned '$GIT_REPO'"
 fi
 
-echo "Setup complete."
+cd $DIR/$GIT_REPO &&
+    print_colored $YELLOW "Moved to '$DIR/$GIT_REPO'"
+
+print_colored $GREEN "Setup complete!"
