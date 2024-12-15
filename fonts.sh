c707d4c7bf9fd3fb779438529b1eaacd3cf1c898
Sam <samme.s90@gmail.com>
2024-12-15 11:07:50 +0100

    Fix fonts.sh permission

--- fonts.sh
+++ fonts.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Download and extract JetBrainsMono Nerd Font
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
-mkdir -p /usr/share/fonts/truetype/JetBrainsMonoNerdFont
+sudo curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+sudo mkdir -p /usr/share/fonts/truetype/JetBrainsMonoNerdFont
 sudo tar -xf JetBrainsMono.tar.xz -C /usr/share/fonts/truetype/JetBrainsMonoNerdFont
-rm JetBrainsMono.tar.xz
+sudo rm JetBrainsMono.tar.xz
 
 # Update font cache
 sudo fc-cache -f -v
+
+# Verify installation
+sudo fc-list | grep "JetBrains"
+echo "Verify that the JetBrainsMono Nerd Font is listed above."
+echo "If it is not listed, then the installation failed..."
