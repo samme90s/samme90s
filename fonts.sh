738f17b7f3ff8116655ff332e0eff291fa9f912b
Sam <samme.s90@gmail.com>
2024-12-15 11:13:45 +0100

    Refactor fonts.sh

--- fonts.sh
+++ fonts.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Download and extract JetBrainsMono Nerd Font
-sudo curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
-sudo mkdir -p /usr/share/fonts/truetype/JetBrainsMonoNerdFont
-sudo tar -xf JetBrainsMono.tar.xz -C /usr/share/fonts/truetype/JetBrainsMonoNerdFont
-sudo rm JetBrainsMono.tar.xz
+FONT="JetBrainsMono"
+
+# Download and extract Nerd Font
+sudo curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${FONT}.tar.xz
+sudo mkdir -p /usr/share/fonts/truetype/${FONT}NerdFont
+sudo tar -xf ${FONT}.tar.xz -C /usr/share/fonts/truetype/${FONT}NerdFont
+sudo rm ${FONT}.tar.xz
 
 # Update font cache
 sudo fc-cache -f -v
 
 # Verify installation
-sudo fc-list | grep "JetBrains"
-echo "Verify that the JetBrainsMono Nerd Font is listed above."
+sudo fc-list | grep $FONT
+echo "Verify that the $FONT Nerd Font is listed above."
 echo "If it is not listed, then the installation failed..."
